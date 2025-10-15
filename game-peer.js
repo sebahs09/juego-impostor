6614e3600476a000349912ef28ae862b29c21d2e
sebas <s.martinez@galileo.edu.ar>
2025-10-15 20:04:13 -0300

    Mejorar compatibilidad movil: mas servidores STUN y meta tags optimizados

--- game-peer.js
+++ game-peer.js
@@ -364,11 +364,16 @@ function createRoom() {
     createRoomBtn.textContent = 'Creando sala...';
     
     peer = new Peer(roomCode, {
+        debug: 2,
         config: {
             iceServers: [
                 { urls: 'stun:stun.l.google.com:19302' },
-                { urls: 'stun:stun1.l.google.com:19302' }
-            ]
+                { urls: 'stun:stun1.l.google.com:19302' },
+                { urls: 'stun:stun2.l.google.com:19302' },
+                { urls: 'stun:stun3.l.google.com:19302' },
+                { urls: 'stun:stun4.l.google.com:19302' }
+            ],
+            iceTransportPolicy: 'all'
         }
     });
     
@@ -426,11 +431,16 @@ function joinRoom() {
     const playerId = generatePlayerId();
     
     peer = new Peer(playerId, {
+        debug: 2,
         config: {
             iceServers: [
                 { urls: 'stun:stun.l.google.com:19302' },
-                { urls: 'stun:stun1.l.google.com:19302' }
-            ]
+                { urls: 'stun:stun1.l.google.com:19302' },
+                { urls: 'stun:stun2.l.google.com:19302' },
+                { urls: 'stun:stun3.l.google.com:19302' },
+                { urls: 'stun:stun4.l.google.com:19302' }
+            ],
+            iceTransportPolicy: 'all'
         }
     });
     
